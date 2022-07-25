192b9038473a24fdbe0aaef227853ee248db9a55
alexuta-andreea <andreea.alexuta@student.unitbv.ro>
2022-07-25 16:53:49 +0300

    Added Bottom Navigation Bar, that switches between the 3 Activities

--- app/src/main/java/com/example/planner/Calendar.kt
+++ app/src/main/java/com/example/planner/Calendar.kt
@@ -1,32 +1,31 @@
 package com.example.planner
 
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 @Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity() {
+class Calendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calendar)
 
         // Initialize and assign variable
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         // Set Home selected
-        bottomNavigationView.selectedItemId = R.id.tasks
+        bottomNavigationView.selectedItemId = R.id.calendar
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.calendar -> {
-                    startActivity(Intent(applicationContext, Calendar::class.java))
+                R.id.tasks -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
                     overridePendingTransition(0, 0)
                     return@OnNavigationItemSelectedListener true
                 }
-                R.id.tasks -> return@OnNavigationItemSelectedListener true
+                R.id.calendar -> return@OnNavigationItemSelectedListener true
                 R.id.notes -> {
                     startActivity(Intent(applicationContext, Notes::class.java))
                     overridePendingTransition(0, 0)
--- app/src/main/java/com/example/planner/Notes.kt
+++ app/src/main/java/com/example/planner/Notes.kt
@@ -1,22 +1,21 @@
 package com.example.planner
 
 import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 @Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity() {
+class Notes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_notes)
 
         // Initialize and assign variable
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         // Set Home selected
-        bottomNavigationView.selectedItemId = R.id.tasks
+        bottomNavigationView.selectedItemId = R.id.notes
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -26,9 +25,9 @@ class MainActivity : AppCompatActivity() {
                     overridePendingTransition(0, 0)
                     return@OnNavigationItemSelectedListener true
                 }
-                R.id.tasks -> return@OnNavigationItemSelectedListener true
-                R.id.notes -> {
-                    startActivity(Intent(applicationContext, Notes::class.java))
+                R.id.notes -> return@OnNavigationItemSelectedListener true
+                R.id.tasks -> {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
                     overridePendingTransition(0, 0)
                     return@OnNavigationItemSelectedListener true
                 }
