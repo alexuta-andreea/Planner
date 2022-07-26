72c281d2ddcbb84dcdd9641ceef9e5cae1344a49
alexuta-andreea <andreea.alexuta@student.unitbv.ro>
2022-07-26 22:58:25 +0300

    Added more code for Tasks implementation + other things

--- app/src/main/java/com/example/planner/MainActivity.kt
+++ app/src/main/java/com/example/planner/MainActivity.kt
@@ -3,14 +3,34 @@ package com.example.planner
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var taskAdapter: TaskAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        taskAdapter = TaskAdapter(mutableListOf())
+
+        rvTask.adapter = taskAdapter
+        rvTask.layoutManager = LinearLayoutManager(this)
+
+        AddTask.setOnClickListener {
+            val todoTitle = Task.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val task = Task(todoTitle)
+                taskAdapter.addTodo(task)
+                Task.text.clear()
+            }
+        }
+        DeleteDone.setOnClickListener {
+            taskAdapter.deleteDoneTodos()
+        }
 
         // Initialize and assign variable
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
