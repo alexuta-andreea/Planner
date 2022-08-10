625d953b18d0b1d7311864db726c42526921881b
alexuta-andreea <andreea.alexuta@student.unitbv.ro>
2022-08-10 17:45:08 +0300

    Updated Calendar

--- app/src/main/java/com/example/planner/DayViewContainer.kt
+++ app/src/main/java/com/example/planner/DayViewContainer.kt
@@ -5,7 +5,7 @@ import android.widget.TextView
 import com.kizitonwose.calendarview.ui.ViewContainer
 
 class DayViewContainer(view: View) : ViewContainer(view) {
-    val textView = view.findViewById<TextView>(R.id.calendarDayText)
+    val textView = view.findViewById<TextView>(R.id.exThreeDayText)
 
     // With ViewBinding
     // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
